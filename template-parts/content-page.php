2c307bfbbc694a46ec7e71a5c824c1bec14da319
Arthur Kho Caayon <islandjoe@gmail.com>
2015-08-11 00:29:08 +0300

    Replace text domain _s with _t

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,22 +10,22 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+  <header class="entry-header">
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+  </header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+  <div class="entry-content">
+    <?php the_content(); ?>
+    <?php
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_t' ),
+        'after'  => '</div>',
+      ) );
+    ?>
+  </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', '_s' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
+  <footer class="entry-footer">
+    <?php edit_post_link( esc_html__( 'Edit', '_t' ), '<span class="edit-link">', '</span>' ); ?>
+  </footer><!-- .entry-footer -->
 </article><!-- #post-## -->
 
