973255be14c2b6722a38c329037dea6b9d544168
Arthur Kho Caayon <islandjoe@gmail.com>
2015-08-14 13:28:20 +0300

    Cleaned up index.

--- index.php
+++ index.php
@@ -3,4 +3,9 @@
 $context = Timber::get_context();
 $context[ 'posts' ] = Timber::get_posts();
 
+$context[ 'is_home' ]       = is_home();
+$context[ 'the_front_page' ] = is_front_page();
+$context[ 'single_post_title' ] = single_post_title( '', false );
+$context[ 'posts_nav' ] = Timber_s::get_the_posts_navigation();
+
 Timber::render( 'index.twig', $context );
