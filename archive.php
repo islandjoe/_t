ac5df03e15614b11edcdc1a8e3848c6d99f1e9f8
Arthur Kho Caayon <islandjoe@gmail.com>
2015-08-14 12:28:47 +0300

    Cleaned up archive.

--- archive.php
+++ archive.php
@@ -10,4 +10,9 @@
 $context = Timber::get_context();
 $context[ 'posts' ] = Timber::get_posts();
 
+$context[ 'page_title' ]    = Timber_s::get_the_archive_title( '<h1 class="page-title">', '</h1>' );
+$context[ 'taxonomy_desc' ] = Timber_s::get_the_archive_description( '<div class="taxonomy-description">', '</div>' );
+
+$context[ 'posts_nav' ] = Timber_s::get_the_posts_navigation();
+
 Timber::render( 'archive.twig', $context );
