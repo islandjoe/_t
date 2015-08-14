386957750703e78f51400259f2ce1a62c27b1623
Arthur Kho Caayon <islandjoe@gmail.com>
2015-08-14 19:12:30 +0300

    Cleaned up single including comments.

--- 404.php
+++ 404.php
@@ -6,33 +6,50 @@
  *
  * @package _t
  */
+$t = Timber_s::TEXT_DOMAIN;
+
 $context = Timber::get_context();
-$context[ 'page_title' ] = esc_html( 'Oops! That page can&rsquo;t be found.', Timber_s::TEXT_DOMAIN );
-$context[ 'page_content' ] = esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', Timber_s::TEXT_DOMAIN );
-$context[ 'search_form' ] = get_search_form( false );
-$context[ 'recent_post_widget' ] = Timber_s::get_the_widget( 'WP_Widget_Recent_Posts' );
+
+$_title = 'Oops! That page can&rsquo;t be found.';
+$context[ 'page_title' ] = esc_html( $_title, $t );
+
+$_content = 'It looks like nothing was found at this location. Maybe try one of
+             the links below or a search?';
+$context[ 'page_content' ] = esc_html( $_content, $t );
+
+$context[ 'search_form' ]  = get_search_form( false );
+
+$_widget = 'WP_Widget_Recent_Posts';
+$context[ 'recent_post_widget' ] = Timber_s::get_the_widget( $_widget );
 
 // Widgets
 $context[ 'has_multi_categories' ] = $has_multi_categories = _t_categorized_blog();
 
 if ( $has_multi_categories ):
 
-  $context[ 'widget_title' ] = esc_html( 'Most Used Categories', Timber_s::TEXT_DOMAIN );
+  $context[ 'widget_title' ] = esc_html( 'Most Used Categories', $t );
 
   $args = [
-    'orderby' => 'count',
-    'order' => 'DESC',
-    'show_count' => 1,
-    'title_li' => '',
-    'number' => 10
-  ];
+    'orderby'     => 'count',
+    'order'       => 'DESC',
+    'show_count'  => 1,
+    'title_li'    => '',
+    'number'      => 10, ];
   $context[ 'categories_list' ] = Timber_s::list_the_categories( $args );
 
 endif;
 
-$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', '_s' ), convert_smilies( ':)' ) ) . '</p>';
-$context[ 'archives_widget' ] = Timber_s::get_the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+$_str             = 'Try looking in the monthly archives. %1$s';
+$_content         = esc_html__( $_str, $t );
+$_conv            = convert_smilies( ':)' );
+$archive_content  = '<p>' . sprintf( $_content, $_conv ) . '</p>';
 
-$context[ 'tag_cloud_widget' ] = Timber_s::get_the_widget( 'WP_Widget_Tag_Cloud' );
+$_arg1   = 'dropdown=1';
+$_arg2   = "after_title=</h2>$archive_content";
+$_widget = 'WP_Widget_Archives';
+$context[ 'archives_widget' ]  = Timber_s::get_the_widget( $_widget, $_arg1, $_arg2 );
+
+$_widget = 'WP_Widget_Tag_Cloud';
+$context[ 'tag_cloud_widget' ] = Timber_s::get_the_widget( $_widget );
 
 Timber::render( '404.twig', $context );
--- single.php
+++ single.php
@@ -1,6 +1,38 @@
 <?php
+/**
+* The template for displaying all single posts.
+*
+* @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+*
+* @package _t
+*/
 
+/* *Seeing that this is not the:
+//    1) home page, 2) 404 page, 3) search page
+//  $context will inject comments-related vars here, as defined in
+//  'inc/timber.php'
+*/
 $context = Timber::get_context();
 $context[ 'post' ] = $post = Timber::get_post();
+
+$context[ 'posts_nav' ] = Timber_s::get_the_posts_navigation();
+
+// Comments
+$t = Timber_s::TEXT_DOMAIN;
+$prev_lbl = esc_html( 'Comment navigation', $t );
+$next_lbl = esc_html( 'Newer Comments', $t );
+
+$context[ 'sr_text' ] = esc_html( 'Comment navigation', $t );
+$context[ 'comments_link_prev' ] = Timber_s::get_comments_link( 'prev', $prev_lbl );
+$context[ 'comments_link_next' ] = Timber_s::get_comments_link( 'next', $next_lbl );
+
+$list_arg = [
+  'style'      => 'ol',
+  'short_ping' => true
+];
+$context[ 'comments_list' ] = Timber_s::list_the_comments( $list_arg, get_comments() );
+$context[ 'no_comments' ] = esc_html( 'Comments are closed.', $t );
+
+$context[ 'comment_form' ] = Timber_s::get_the_comment_form( $post->ID );
 
 Timber::render( 'single.twig', $context );
