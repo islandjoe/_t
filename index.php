20187f2ea64b7f0d60e09d5a2b15de551ad49b40
Arthur Kho Caayon <islandjoe@gmail.com>
2015-08-15 11:10:49 +0300

    Create places for context vars according to its proper patron file. - content.twig - content-none.twig

--- index.php
+++ index.php
@@ -10,4 +10,27 @@ $context[ 'the_front_page' ] = is_front_page();
 $context[ 'single_post_title' ] = single_post_title( '', false );
 $context[ 'posts_nav' ] = Timber_s::get_the_posts_navigation();
 
+// Vars for <content.twig>:
+$context[ 'post_class' ] = get_post_class();
+$_arg = [
+  'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+  'after'  => '</div>' ];
+$context[ 'link_pages' ]   = Timber_s::get_the_link_pages( $_arg );
+$context[ 'entry_footer' ] = Timber_s::capture_output( '_t_entry_footer', [] );
+
+// Vars for <content-none.twig>:
+$context[ 'can_publish_posts' ] = current_user_can( 'publish_posts' );
+$context[ 'create_post_link' ] = Timber_s::get_create_post_link();
+$context[ 'is_search' ] = is_search();
+$context[ 'no_match_found' ] = esc_html(
+  'Sorry, but nothing matched your search terms. Please try again with some different keywords.',
+  $t
+);
+$context[ 'no_content_found' ] = esc_html(
+  'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching
+      can help.', $t
+);
+$context[ 'search_form' ] = Timber_s::get_the_search_form();
+
+
 Timber::render( 'index.twig', $context );
