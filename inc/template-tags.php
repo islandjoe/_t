9f883dc3cb023c9907bee0364952417e843b5566
Arthur Kho Caayon <islandjoe@gmail.com>
2015-08-15 11:04:42 +0300

    Minor formatting

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -85,12 +85,14 @@ function _t_posted_on() {
 
   $posted_on = sprintf(
     esc_html_x( 'Posted on %s', 'post date', '_t' ),
-    '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+    '<a href="'.esc_url( get_permalink() ).'" rel="bookmark">'.$time_string.'</a>'
   );
 
   $byline = sprintf(
     esc_html_x( 'by %s', 'post author', '_t' ),
-    '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+    '<span class="author vcard"><a class="url fn n" href="' .
+      esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' .
+      esc_html( get_the_author() ) . '</a></span>'
   );
 
   echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
