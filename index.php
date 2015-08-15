3a9dab78f975cf24c529a2fca37011ee75011edb
Arthur Kho Caayon <islandjoe@gmail.com>
2015-08-15 11:07:24 +0300

    Rearrange 'posts' context var into its logical place.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
 $context = Timber::get_context();
-$context[ 'posts' ] = Timber::get_posts();
 
 $context[ 'is_home' ]       = is_home();
+
+// Vars for <index.twig>:
+$context[ 'posts' ]             = Timber::get_posts();
 $context[ 'the_front_page' ] = is_front_page();
 $context[ 'single_post_title' ] = single_post_title( '', false );
 $context[ 'posts_nav' ] = Timber_s::get_the_posts_navigation();
